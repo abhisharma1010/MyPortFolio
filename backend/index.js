7563107cd1250796dce7e2da61bca50e940f1f7d
SohamKumar123 <rajivranjanktq@gmail.com>
2024-09-19 16:58:36 +0530

    yes commit again

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,14 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 // import port number from .env file
-require('dotenv').config();
-//  const PORT = process.env.PORT || 3000;
+// require('dotenv').config();
+// const PORT = process.env.PORT || 3000;
 
 // middleware to parse json request body
 app.use(cors());
 app.use(express.json());
+
+
 
 // connect to the database
 require("./config/database").connect();
@@ -21,8 +23,9 @@ app.listen(4000,()=>{
     try{
         console.log(`Server is running on port 4000`);
     }catch(err){
-        console.log("Yhi dikkat hai");   
+        console.log("Server issue");   
     }
+   
     
 });
 app.get('/',(req,res)=>{
